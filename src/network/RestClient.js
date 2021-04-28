4d66ea10163831bfc6d54db20cdacf1b29b5b43d
ahmadmustafeen <engr.ahmadmustafeen@gmail.com>
2021-04-28 11:48:39 +0500

    initial setup with all the must required dependencies

---
+++ src/network/RestClient.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/named */
+import { create } from 'apisauce';
+import { BASE_URL, API_VERSION } from '_constants/Network';
+
+// Rest Client for FuelShip Mobile App
+export const RestClient = create({
+    baseURL: `${BASE_URL}/${API_VERSION}`,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: '',
+    },
+    timeout: 30000,
+});
